bad0113b3d5d3020694e7f423b88a98bfb9f53b6
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-08-04 19:15:16 -0600

    wip

--- src/GSuite.php
+++ src/GSuite.php
@@ -5,53 +5,37 @@ namespace Wyattcast44\GSuite;
 class GSuite
 {
     /**
-     * The config file
-     */
-    protected $config;
-
-    /**
      * The Google Client
      */
     public $google_client;
 
     /**
      * Bootstrap the service
-     * @return GSuite
+     * @return $this
      */
     public function __construct()
     {
-        $this->setConfig()->setGoogleClient();
-
-        return $this;
-    }
-
-    /**
-     * Set the configuration file
-     * @return GSuite
-     */
-    protected function setConfig()
-    {
-        $this->config = config('gsuite');
+        $this->setGoogleClient();
 
         return $this;
     }
 
     /**
      * Set the $google_client
-     * @return GSuite
+     * @return $this
      */
     protected function setGoogleClient()
     {
-        // Set the credentials in the env
         if (!getenv('GOOGLE_APPLICATION_CREDENTIALS')) {
-            putenv('GOOGLE_APPLICATION_CREDENTIALS=' . $this->config['credentials_path']);
+            putenv('GOOGLE_APPLICATION_CREDENTIALS=' . config('gsuite.credentials_path'));
         }
 
         // Set and configure the client
-        $this->google_client = new \Google_Client();
-        $this->google_client->useApplicationDefaultCredentials();
-        $this->google_client->setSubject($this->config['subject']);
-        $this->google_client->setScopes($this->config['scopes']);
+        $this->google_client = tap(new \Google_Client, function ($google_client) {
+            $google_client->useApplicationDefaultCredentials();
+            $google_client->setSubject(config('gsuite.subject'));
+            $google_client->setScopes(config('gsuite.scopes'));
+        });
 
         return $this;
     }
