966c1ecab2430a1f4fd1ba2816b31d91896fb25a
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-08-07 19:34:01 -0600

    wip

--- src/Services/Accounts/AccountsRepo.php
+++ src/Services/Accounts/AccountsRepo.php
@@ -15,6 +15,20 @@ class AccountsRepo implements AccountsRepoContract
     protected $client;
 
     /**
+     * G-Suite account user first name max length
+     *
+     * @link https://developers.google.com/admin-sdk/directory/v1/reference/users/insert
+     */
+    const MAX_FIRST_NAME_LENGTH = 60;
+
+    /**
+     * G-Suite account user last name max length
+     *
+     * @link https://developers.google.com/admin-sdk/directory/v1/reference/users/insert
+     */
+    const MAX_LAST_NAME_LENGTH = 60;
+
+    /**
      * Bootstrap the client
      *
      * @return self
@@ -83,9 +97,68 @@ class AccountsRepo implements AccountsRepoContract
         return $account;
     }
 
-    public function insert()
+    /**
+     * Create a new G-Suite account
+     *
+     * @link https://developers.google.com/admin-sdk/directory/v1/reference/users/insert
+     *
+     * @param array $name | Should contain: first_name, last_name
+     * @param string $email | The desired email address for the new account, ex joe@email.com
+     * @param string $password | The desired default password
+     * @param bool $changePasswordNextLogin | Default true
+     * @return \GoogleServiceUser
+     */
+    public function insert(array $name, string $email, string $password, bool $changePasswordNextLogin = true)
     {
-        //
+        /**
+         * Ensure $name has proper keys
+         */
+        if (!Arr::has($name, ['first_name', 'last_name'])) {
+            throw new \Exception("Name parameter must contain the following items: first_name, last_name", 1);
+        }
+
+        /**
+         * Ensure names meet max length requirement
+         */
+        if (strlen($name['first_name']) > self::MAX_FIRST_NAME_LENGTH || strlen($name['last_name']) > self::MAX_LAST_NAME_LENGTH) {
+            throw new \Exception("First and last name must be 60 characters or less", 1);
+        }
+
+        /**
+         * Check email availability
+         */
+        if (!$this->checkEmailAvailability($email)) {
+            throw new Exception("Email address already taken.", 1);
+        }
+
+        /**
+         * Create and set the Google Directory Name
+         */
+        $directory_name = tap(new \Google_Service_Directory_UserName, function ($directory_name) use ($name) {
+            $directory_name->setGivenName(ucfirst($name['first_name']));
+            $directory_name->setFamilyName(ucfirst($name['last_name']));
+        });
+
+        /**
+         * Create and configure the new Google User
+         */
+        $google_user = tap(new \GoogleServiceUser, function ($google_user) use ($directory_name, $email, $password, $changePasswordNextLogin) {
+            $google_user->setName($directory_name);
+            $google_user->setPrimaryEmail($email);
+            $google_user->setPassword($password);
+            $google_user->setChangePasswordAtNextLogin($changePasswordNextLogin);
+        });
+
+        /**
+         * Attempt to actually create the new GSuite account
+         */
+        try {
+            $account = $this->client->insert($google_user);
+        } catch (\Exception $e) {
+            throw new \Exception("Error creating new G-Suite account.", 1);
+        }
+
+        return $account;
     }
 
     // $parameters = [
