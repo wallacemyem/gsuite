ab2c16a0a94328b263f4aeac6f20d8565f658017
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-08-08 15:12:35 -0600

    wip

---
+++ tests/GoogleClientTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Wyattcast44\Gsuite\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class GoogleClientTest extends TestCase
+{
+    public function test_true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
