3b04eea4397380d32db68d6952fe695655e9f31f
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-08-09 23:58:11 -0600

    wip

--- src/Actions/SuspendAccountAction.php
+++ src/Actions/SuspendAccountAction.php
@@ -2,11 +2,14 @@
 
 namespace Wyattcast44\GSuite\Actions;
 
+use Spatie\QueueableAction\QueueableAction;
 use Wyattcast44\GSuite\Resources\Accounts\GSuiteAccount;
 use Wyattcast44\GSuite\Resources\Accounts\AccountsRepository;
 
 class SuspendAccountAction
 {
+    use QueueableAction;
+
     protected $repository;
 
     public function __construct(AccountsRepository $accounts_repo)
